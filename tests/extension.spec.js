3ac831423696dfcf2465e2d57b9bcb91734183e8
Anna Ristau <annaristau@MacBookPro.attlocal.net>
2025-03-16 18:17:43 -0400

    Add a backgorund script to test extension to try to resolve timeout issue

--- tests/extension.spec.js
+++ tests/extension.spec.js
@@ -1,16 +1,12 @@
 // @ts-check
 import { test, expect } from '../fixtures/extensionTest';
 
-test('has title2', async ({ page }) => {
+test('example test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-
-
-// test('extension popup', async ({ page, extensionId }) => {
-//     await page.goto(`chrome-extension://${extensionId}/popup.html`);
-//     await expect(page.locator('body')).toHaveText('my-extension popup');
-// });
+test('popup page', async ({ page, extensionId }) => {
+  await page.goto(`chrome-extension://${extensionId}/popup.html`);
+  await expect(page.locator('body')).toHaveText('my-extension popup');
+});

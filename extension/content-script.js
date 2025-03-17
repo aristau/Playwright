8d2b3f17d943a5e4ebfd2ff932d73491eb2850cf
Anna Ristau <annaristau@MacBookPro.attlocal.net>
2025-03-17 19:09:11 -0400

    Added code to test extension to make it modify first name input on a page

---
+++ extension/content-script.js
@@ -0,0 +1,7 @@
+//This script defines what the extension does
+
+//Fill in the first name textbox on the website
+document.getElementById('firstName').value = 'Anna';
+
+
+
--- tests/extension.spec.js
+++ tests/extension.spec.js
@@ -1,20 +1,35 @@
 // @ts-check
 import { test, expect } from '../fixtures/extensionTest';
 
-test('example test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
+// test('example test', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('extension test', async ({ page }) => {
+//   await page.goto('chrome://extensions');
+//   await expect(page).toHaveTitle(/Extensions/);
+// });
+
+test('view extension popup', async ({ page }) => {
+  await page.goto('chrome-extension://apoakkflmfmppaebpefjflbhkhbkojom/hello.html');
+  await expect(page.locator('body')).toContainText('Hello Extensions')
+ });
+
+ test('first name is populated', async ({ page }) => {
+  await page.goto('https://qa-practice.netlify.app/register');
+  await expect(page.getByRole('textbox', {name: 'First Name'})).toHaveValue('Anna')
 });
 
-test('extension test', async ({ page }) => {
-  await page.goto('chrome://extensions');
-  await expect(page).toHaveTitle(/Extensions/);
-});
+// test('first name has correct value', async({page }) => {
 
-test('extension test2', async ({ page }) => {
-  await page.goto('chrome://extensions/?id=apoakkflmfmppaebpefjflbhkhbkojom');
-  await expect(page).toHaveTitle(/Extensions/);
-});
+// });
+
+
+// test('required fields are populated', async ({ page}) => {
+//   await page.goto('https://qa-practice.netlify.app/register');
+//   await expect(page.locator('input[required]')).toHaveCount(2);
+// });
 
 
 // test('extension test3', async ({ page, extensionId }) => {

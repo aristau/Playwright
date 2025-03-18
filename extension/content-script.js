8ab07a1ef4d315417dcee839054304cd74e21ab2
Anna Ristau <annaristau@MacBookPro.attlocal.net>
2025-03-18 14:32:50 -0400

    Modified extension to save data to Chrome local storage

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,7 +1,25 @@
 //This script defines what the extension does
 
-//Fill in the first name textbox on the website
-document.getElementById('firstName').value = 'Anna';
+//Fill in the first name textbox on a website with the firstName that was saved in the extension
+chrome.storage.sync.get(["extensionFirstName"]).then((result) => {
+    console.log("Extension First Name is " + result.extensionFirstName);
+    var extensionFirstName = result.extensionFirstName;
+    if(typeof extensionFirstName !== "undefined"){
+        document.getElementById('firstName').value = extensionFirstName;
+    }
+    else{
+        document.getElementById('firstName').value = 'Anna';
+    }
+});
+
+//Todo: You have to refresh after saving the name in the extension. 
+
+
+
+
+
+
+
 
 
 
--- tests/extension.spec.js
+++ tests/extension.spec.js
@@ -1,30 +1,16 @@
 // @ts-check
+import { chromium } from '@playwright/test';
 import { test, expect } from '../fixtures/extensionTest';
-
-// test('example test', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-
-// test('extension test', async ({ page }) => {
-//   await page.goto('chrome://extensions');
-//   await expect(page).toHaveTitle(/Extensions/);
-// });
 
 test('view extension popup', async ({ page }) => {
   await page.goto('chrome-extension://apoakkflmfmppaebpefjflbhkhbkojom/hello.html');
-  await expect(page.locator('body')).toContainText('Hello Extensions')
+  await expect(page.locator('body')).toContainText('Hello Extensions');
  });
 
  test('first name is populated', async ({ page }) => {
   await page.goto('https://qa-practice.netlify.app/register');
-  await expect(page.getByRole('textbox', {name: 'First Name'})).toHaveValue('Anna')
+  await expect(page.getByRole('textbox', {name: 'First Name'})).toHaveValue('Anna');
 });
-
-// test('first name has correct value', async({page }) => {
-
-// });
-
 
 // test('required fields are populated', async ({ page}) => {
 //   await page.goto('https://qa-practice.netlify.app/register');
